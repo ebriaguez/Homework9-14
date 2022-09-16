79bf69f33a087dacec146d6bb817507b15863d72
Emily Briaguez-Agurto <emilyb.agurto@gmail.com>
2022-09-16 09:51:12 -0400

    pseudo coding

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -23,13 +23,22 @@ public class Greeting {
 //        //Username enter will input
 //        storedInfo = userOne.nextLine();
 
+
+        //putting everything in a while loop to loop through the user input
         while (true){
+            //creating a new object that is stored in userOne
             Scanner userOne = new Scanner(System.in);
+            //print out inputUser
             System.out.println(inputUser);
+            //store user input in storedInfo
             storedInfo = userOne.nextLine();
+            //create an if statement to print only Alice or Bob
             if (storedInfo.equals("Alice") || storedInfo.equals("alice")){
+                //sys out output message and storedInfo
                 System.out.println(outputUser + storedInfo + "!");
+                //break keyword to stop if conditions are met
                 break;
+                //continuing statements
             } else if (storedInfo.equals("Bob") || storedInfo.equals("bob")){
                 System.out.println(outputUser + storedInfo + "!");
                 break;
